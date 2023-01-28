21454b1d9ae90151988d62ea47f9705f84cc7896
VT-Sase <vtsase@gmail.com>
2023-01-28 18:47:10 -0500

    01/28/23: Removed Unused JS Code

--- js/script.js
+++ js/script.js
@@ -1,27 +1,7 @@
+//Hamburger Drop-Down Menu Functionality
 
 $(document).ready(function(){
     $("#hamburger").click(function() {
         $(".navbar-hidden").slideToggle(300);
     });
-
-    // $(".owl-carousel").owlCarousel({
-    //     items: 1,
-    //     autoplay: true,
-    //     autoplayTimeout: 3000,
-    //     loop: true
-    // });
 });
-  
-
-// var images = ["/img/slideshow/6.jpg", "/img/slideshow/7.jpg", "/img/slideshow/2.jpg", "/img/slideshow/4.jpg", "/img/slideshow/3.jpg", "/img/slideshow/6.jpg", "/img/slideshow/1.jpg", "/img/slideshow/5.jpg"];
-// var i = 1;
-// setInterval(function(){
-//     var slideshow = document.querySelector(".slideshow");
-
-//     slideshow.style.backgroundImage = "url(" + images[i] + ")";
-//     i = i + 1;
-//     if (i == images.length) {
-//       i =  0;
-//     }
-
-// }, 6000);
