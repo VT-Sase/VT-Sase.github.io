c6c5510f3817e5c9774e04bf1a990695de485f96
Andrew Hoang Tran <andrewtranva@gmail.com>
2023-03-29 00:44:06 -0400

    Griffin O. Helped Me Fix and Optimize the Carousel for Dynamic Selecting of Images (#1)
    * 03/29/23: Removed Extra Zip Files of Images
    
    * 03/29/23: Griffin O. Helped Me Fixed and Optimized the Carousel for the Home index.html Page
    
    ---------
    
    Co-authored-by: VT-Sase <vtsase@gmail.com>

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ $(document).ready(function(){
         $(".navbar-hidden").slideToggle(300);
     });
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: false,
+        height: 100,
+        items: 1
+    });
+});
