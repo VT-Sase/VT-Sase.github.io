fdad8e51c5cbf84ba4752552d45a2b3a021138d3
AndrewTran03 <andrewtranva@gmail.com>
2023-04-16 00:51:14 -0400

    04/16/23: Cleaned Up Commenting and Changed 'setTimeout()' Function Time Waiting to be 5000ms = 5sec to Avoid Issues with Home index.html Page Loading

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
-function overrideFunctionality() {
-    
-    // Hamburger Drop-Down Menu Functionality
+// For Home index.html Page
+function overrideFunctionality()
+{
+    // Overriding Default Hamburger Drop-Down Menu Functionality (Mobile)
     $(document).ready(function(){
         $("#hamburger").click(function() {
             $(".navbar-hidden").slideToggle(300);
         });
     });
-
 
     // Overriding Default Owl-Carousel Functionality
     $(document).ready(function() {
@@ -21,4 +21,6 @@ function overrideFunctionality() {
     });
 }
 
-setTimeout(overrideFunctionality, 3000);
+// Reduces (Does Not 100% Avoid It Though) the Chance of the JavaScript Not 
+// Loading the Carousel By the Time of the Initial Page Load
+setTimeout(overrideFunctionality, 5000);
