e607154c27df1a9e2f2f0ccd290833b11343d44e
AndrewTran03 <andrewtranva@gmail.com>
2023-04-21 13:03:53 -0400

    04/21/23: Added a Started/Incomplete Draft of the New/Beta 'Analytics' Page for the SASE Website

---
+++ js/data_analytics.js
@@ -0,0 +1,22 @@
+// Imports Currently Do Not Work Right Now (Needs to be Fixed and Page Needs
+// to be Created)
+
+/*
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+window.THREE = THREE;
+
+const scene = THREE.Scene;
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader();
+
+function test()
+{
+    // controls.rotate;
+    console.log("Hello");
+}
+
+test();
+*/
