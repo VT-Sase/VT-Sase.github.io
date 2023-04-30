c845d132a4c3ddb56f30e2a1f7fe3466b5d93bac
AndrewTran03 <andrewtranva@gmail.com>
2023-04-30 03:40:43 -0400

    04/30/23: Added Arrows on Top of Carousel, Fixed Home Page Display Photo to be More Professional and Appropriate, and Added a Card Honoring the Webmaster at the Bottom of the Home index.html Page

--- js/script.js
+++ js/script.js
@@ -23,34 +23,38 @@ function overrideFunctionality()
                 autoplayTimeout: 5000,
                 autoplayHoverPause: true,
                 items: 1,
-                nav: false,
-                dots: true
+                dots: false,
+                nav: true,
+                navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+                // navText: ["<i class='fa fa-chevron-left'></i><i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>", "<i class='fa fa-chevron-right'></i><i class='fa fa-chevron-right'></i>"]
+                // navText: ["<div class='owl-carousel__nav'><div class='owl-carousel__fast_forward_prev'><i class='fa fa-chevron-left'></i><i class='fa fa-chevron-left'></i></div><div class='owl-carousel__prev'><i class='fa fa-chevron-left'></i></div><div class='owl-carousel__next'><i class='fa fa-chevron-right'></i></div><div class='owl-carousel__fast_forward_next'><i class='fa fa-chevron-right'></i><i class='fa fa-chevron-right'></i></div></div>"]
             });
     
             // Custom Navigation to Fast-Forward Previous, Previous, Next, Fast-Forward Next Image in Carousel
-            const fastBackwordForwardDuration = 5;
+            // (Below the Carousel)
+            // const fastBackwordForwardDuration = 5;
     
-            $('.owl-carousel__fast_forward_prev').click(() => {
-                for (let i = 0; i < fastBackwordForwardDuration; i++)
-                {
-                    owl.trigger('prev.owl.carousel');
-                }
-            });
+            // $('.owl-carousel__fast_forward_prev').click(() => {
+            //     for (let i = 0; i < fastBackwordForwardDuration; i++)
+            //     {
+            //         owl.trigger('prev.owl.carousel');
+            //     }
+            // });
     
-            $('.owl-carousel__prev').click(() => {
-                owl.trigger('prev.owl.carousel');
-            });
+            // $('.owl-carousel__prev').click(() => {
+            //     owl.trigger('prev.owl.carousel');
+            // });
         
-            $('.owl-carousel__next').click(() => {
-                owl.trigger('next.owl.carousel');
-            });
+            // $('.owl-carousel__next').click(() => {
+            //     owl.trigger('next.owl.carousel');
+            // });
     
-            $('.owl-carousel__fast_forward_next').click(() => {
-                for (let i = 0; i < fastBackwordForwardDuration; i++)
-                {
-                    owl.trigger('next.owl.carousel');
-                }
-            });
+            // $('.owl-carousel__fast_forward_next').click(() => {
+            //     for (let i = 0; i < fastBackwordForwardDuration; i++)
+            //     {
+            //         owl.trigger('next.owl.carousel');
+            //     }
+            // });
         }
     });
 }
