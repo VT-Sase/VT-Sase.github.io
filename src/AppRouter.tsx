0b5cb4f0e00a8b3fe4f4ff89c904c41ea81f2395
Andrew Tran <andrewtranva@gmail.com>
2024-03-01 17:21:31 -0500

    Removed extra imports from AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Example } from "./components";
 
 const AppRouter: React.FC = () => {
