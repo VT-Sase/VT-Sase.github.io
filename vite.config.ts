797255f25047d7b27b48d58b0954b052a01ec0f6
Andrew Tran <andrewtranva@gmail.com>
2024-03-19 23:21:59 -0400

    Added 'build' configuration in the Vite frontend

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1500 // Set the chunk size warning limit to 1500 kB
+  }
 });
