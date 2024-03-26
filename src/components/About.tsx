53158855bd4c644f2567c37e3158165071998a8d
Joe <joedo2910@vt.edu>
2024-03-26 01:54:23 -0400

    cleaned up unused comments

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,3 @@
-//import '@fontsource/roboto/300.css';
-//import '@fontsource/roboto/400.css';
-//import '@fontsource/roboto/500.css';
-//import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
 
 function PicAndText () {
