2410b6467d0a726616e421ebeec6c54e27766d43
EnshaalRajput <enshaal8528@gmail.com>
2024-03-26 01:13:57 -0400

    Update About.tsx
    added text and function to see how it would show

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,23 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+//import '@fontsource/roboto/300.css';
+//import '@fontsource/roboto/400.css';
+//import '@fontsource/roboto/500.css';
+//import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
 
+function PicAndText () {
+  return <h1>using a function</h1>;
+}
+
 const About: React.FC = () => {
-  return <><Button variant="contained">Hello world</Button><h1>SASE!!!</h1></>;
+  return <>
+    <Button variant="contained">Hello world</Button>
+    <h1>SASE About Page!</h1>
+    <hr></hr>
+    <p>This will be an example paragraph. <br></br>
+      This will be on the second line of the paragraph.</p>
+    <PicAndText />
+    <PicAndText></PicAndText>
+  </>;
 };
 
 export default About;
