08c7efe0a7444e77d1a13d9c10e2d4478f00cde8
Peter Do <130791427+pdoo2004@users.noreply.github.com>
2024-03-31 13:50:04 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,28 @@
+import footerImage from '../assets/Logo.png';
+
+
 const Footer: React.FC = () => {
-  return <></>;
+  return (
+    <footer style={{
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: '#000',
+      color: '#fff',
+      padding: '10px 20px',
+      position: 'fixed',
+      left: '0',
+      bottom: '0',
+      width: '100%',
+      boxSizing: 'border-box', 
+    }}>
+      <img src={footerImage} alt="Footer Logo" style={{ maxWidth: '100px' }} />
+      <div style={{ textAlign: 'right' }}>
+        © 2024 SASE at Virginia Tech<br />
+        Society of Asian Scientists and Engineers
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
