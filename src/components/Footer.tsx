da084d5f0dadc2c5e444f577a8d0c5ebda247823
Peter Do <130791427+pdoo2004@users.noreply.github.com>
2024-04-03 16:32:10 -0400

    Colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,21 @@
 import footerImage from '../assets/Logo.png';
 
-
 const Footer: React.FC = () => {
   return (
     <footer style={{
       display: 'flex',
       justifyContent: 'space-between',
       alignItems: 'center',
-      backgroundColor: '#000',
-      color: '#fff',
+      backgroundColor: '#e8effa', 
+      color: '#919090', 
       padding: '10px 20px',
       position: 'fixed',
       left: '0',
       bottom: '0',
       width: '100%',
-      boxSizing: 'border-box', 
+      boxSizing: 'border-box',
+      fontFamily: '"Lato", sans-serif', 
+      fontWeight: '650', 
     }}>
       <img src={footerImage} alt="Footer Logo" style={{ maxWidth: '100px' }} />
       <div style={{ textAlign: 'right' }}>
