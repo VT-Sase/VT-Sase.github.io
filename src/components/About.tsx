ef6daadabf435a200cbcff5679ffc86d93b1b703
EnshaalRajput <enshaal8528@gmail.com>
2024-04-05 17:44:34 -0400

    added cards

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,19 +4,72 @@
 //import '@fontsource/roboto/700.css';
 import Button from '@mui/material/Button';
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 function PicAndText () {
+  <Button variant="contained">Hello world</Button>
   return <h1>using a function</h1>;
 }
 
 const About: React.FC = () => {
   return <>
-    <Button variant="contained">Hello world</Button>
+    
     <h1>SASE About Page!</h1>
     <hr></hr>
-    <p>This will be an example paragraph. <br></br>
-      This will be on the second line of the paragraph.</p>
     <PicAndText />
-    <PicAndText></PicAndText>
+    <Card>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Professional Events
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Sase has Professional Events (will elaborate later)
+          </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        height="200"
+        image="vanilla-sase-website\assets\img\about\prof.jpg"
+        alt="Professional events"
+      />
+    </Card>
+
+    <Card>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Mentor Mentee Program
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+             (will elaborate later)
+          </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        height="200"
+        image="vanilla-sase-website\assets\img\about\2022-23-Mentor-Mentee-Display-Image.jpg"
+        alt="Professional events"
+      />
+    </Card>
+
+    <Card>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+            Community Service
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            (will elaborate later)
+          </Typography>
+      </CardContent>
+      <CardMedia
+        component="img"
+        height="200"
+        image="vanilla-sase-website\assets\img\about\service-updated-big-event.png"
+        alt="Professional events"
+      />
+    </Card>
   </>;
 };
 
