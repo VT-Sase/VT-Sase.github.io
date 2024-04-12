62e061b2af8aa56f90a04e70039fac75b48d81a9
AashishAryal1 <aashisha@vt.edu>
2024-04-12 18:17:17 -0400

    Added link back to homepage when page not found

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const NotFound: React.FC = () => {
   return (
     <div>
       <h1>Page Not Found</h1>
+      <p>Go To <Link to="/">Home Page</Link></p>
     </div>
   );
 };
