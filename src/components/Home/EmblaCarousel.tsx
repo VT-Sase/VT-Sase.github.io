6bd742abfe0414524eaa6c57baa9379629a8092c
Adam Nguyen <AdanatoNw@gmail.com>
2024-04-19 18:01:07 -0400

    Cleaned up carousel for mobile responsive

--- src/components/Home/EmblaCarousel.tsx
+++ src/components/Home/EmblaCarousel.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { EmblaOptionsType } from "embla-carousel";
-import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
 import { PrevButton, NextButton, usePrevNextButtons } from "./EmblaCarouselArrowButtons";
 import useEmblaCarousel from "embla-carousel-react";
 
@@ -12,8 +11,6 @@ const baseImagePath = "src/assets/img/slideshow-compressed-smaller";
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { imageNames, options } = props;
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
-
-  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
 
   const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
 
@@ -33,16 +30,6 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
         <div className="embla__buttons">
           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={"embla__dot".concat(index === selectedIndex ? " embla__dot--selected" : "")}
-            />
-          ))}
         </div>
       </div>
     </section>
