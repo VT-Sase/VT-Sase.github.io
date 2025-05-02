80de579309e170ec6c21bda2c0267ca30d900e5b
tiffanywid <tiffanywid@vt.edu>
2025-05-02 18:32:54 -0400

    Added more YIR images

--- src/components/YearInReview2425.tsx
+++ src/components/YearInReview2425.tsx
@@ -15,18 +15,12 @@ title:
 description:
 */
 
+/*
+  Review Descriptions & Change FB Links for SaseGala, LunarNewYears, 
+*/
 const events: Event[] = [
   {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-final.jpg",
-    link: "https://www.facebook.com/photo?fbid=696990322427723&set=pcb.697032379090184",
-    title: "SASE's Final/End-of-Year GBM - Celebrating our seniors with fun and laughter!",
-    description: "As the year winds down, we enjoyed celebrating our Seniors graduating and" +
-    "moving on the bigger and greater things! Everyone enjoyed the Spongebob-Squarepants theme" +
-    "and the Senior Superlative Paper Plates! From all of us on the SASE Officer Board 2022-2023," +
-    "it's been a great honor to serve this VT Chapter this year, and we look forward to seeing what next year holds!"
-  },
-  {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/sase-gala.jpg",
+    img: "src/assets/img/year-in-review-compressed-smaller/2024-2025/SaseGala.jpg",
     link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.677997954326960",
     title: "SASE's Spring Gala - A banquet filled with delicious food and wonderful performances!",
     description: "Gala hosted this year was off-the-charts! Everyone from" + 
@@ -35,48 +29,16 @@ const events: Event[] = [
     "night alongside old friends as well as had the opportunity to meet some new ones too!"
   },
   {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/sase-coffeehouse-night.jpg",
-    link: "https://www.facebook.com/media/set?vanity=saseatvt&set=a.695184142608341",
-    title: "SASE at AASU Coffeenight",
-    description: "Wow! We saw a great amount of wonderful performances from" +
-    "our very own members as well ones from other Asian organizations! The talent was off the chart! All of our members" +
-    "were shocked at the amount of skill and dedication displayed in the performances!" 
-  },
-  {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-spring.jpg",
-    link: "https://www.facebook.com/media/set?vanity=saseatvt&set=a.695184102608345",
-    title: "SASE's 2nd Mentor-Mentee GBM ",
-    description: "Spring has come! Cherry blossoms, beautiful nature outside, and" +  
-    "the warm weather is here to stay! But also it brings human Bop-It and Origami? This GBM was full of excitement" + 
-    "and delightful surprises!"
-  },
-  {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-lunar-new-year-3.jpg",
+    img: "src/assets/img/year-in-review-compressed-smaller/2024-2025/LunarNewYear.jpg",
     link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.637934774999945",
-    title: "SASE's Lunar New Year GBM (Image 1) ",
-    description: "We welcome our new SASE Interns for the spring" +  
-    "semester! The entire SASE officer board wishes you a Happy Lunar New Year 2023!"
-  },
-  {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-lunar-new-year-2.jpg",
-    link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.637934774999945",
-    title: "SASE's Lunar New Year GBM (Image 2)",
+    title: "SASE's Lunar New Year GBM",
     description: "Our members enjoying the new year, welcoming all of its joys and surprises!"
   },
   {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-lunar-new-year.jpg",
-    link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.637934774999945",
-    title: "SASE's Lunar New Year GBM (Image 3)",
-    description: "Our wonderful Internal Vice-President Seneca" +
-    "tests egg-protecting contraptions made by each of the groups! Most do not succeed, but it's the effort that" +
-    "counts here!👍"
-  },
-  {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-winter.jpg",
+    img: "src/assets/img/year-in-review-compressed-smaller/2024-2025/Thanksgiving.jpg",
     link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.695162109277211",
-    title: "SASE Winter GBM",
-    description: "A night where we hear our wonderful President Duy read" + 
-    "an interesting MadLibs around a roaring fire!"
+    title: "SASE Thanksgiving GBM",
+    description: "A feast surrounded with friends!"
   },
   {
     img: "src/assets/img/year-in-review-compressed-smaller/2024-2025/HalloweenGBM.jpg",
@@ -94,10 +56,10 @@ const events: Event[] = [
     "mentees!"
   },
   {
-    img: "src/assets/img/year-in-review-compressed-smaller/2022-2023/GBM-back-to-school.jpg",
-    link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.521407833319307",
-    title: "GBM #1 - Camp Half-Blood",
-    description: "Our members enjoying the new year, welcoming all of its joys and surprises!"
+    img: "src/assets/img/year-in-review-compressed-smaller/2024-2025/Fashion_Show.jpg",
+    link: "https://www.facebook.com/media/set/?vanity=saseatvt&set=a.1184126177047466",
+    title: "SASE Fashion Show",
+    description: "A showstopping night to remember!"
   },
 
 ];
